4057ac3e891a2d53385d9f4c62d040294f1c56d5
Tung Kah Marn <jiaming506@gmail.com>
2023-09-20 21:53:45 +0800

    numbering

--- 03_uniqueSort.js
+++ 03_uniqueSort.js
@@ -3,13 +3,11 @@
 
 //Prep -> parameters, results, example, pseudo code
 
-
-// parameter: array(numbers, no unique elements, no empty, not sorted, whole numbers, 
+// parameter: array(numbers, no unique elements, no empty, not sorted, whole numbers,
 // result: array(sorted, no empty, arrayresult.length <= array receive.length)
 
 //input: [1, 5, 2, 1] => [1, 2, 5]
 //input: [4, 2, 2, 3, 2, 2, 2] => [2, 3, 4]
-
 
 // function uniqueSort2(arr){
 //     const breadcrumbs = {}
@@ -25,19 +23,20 @@
 //     return uniqueArr.sort((a,b) => a - b)
 // }
 
-function uniqueSort(arr){
-    //remove duplicates
-    const breadcrumbs = {}
+function uniqueSort(arr) {
+  //remove duplicates
+  const breadcrumbs = {};
 
-    for(let num of arr){
-        if(!breadcrumbs[num])
-            breadcrumbs[num] = true
-    }
+  for (let num of arr) {
+    if (!breadcrumbs[num]) breadcrumbs[num] = true;
+  }
 
-    //sort the array through ascending order
-    //return result array
-    return Object.keys(breadcrumbs).sort((a, b) => a - b).map(e => parseInt(e))
+  //sort the array through ascending order
+  //return result array
+  return Object.keys(breadcrumbs)
+    .sort((a, b) => a - b)
+    .map((e) => parseInt(e));
 }
 
-console.log(uniqueSort2([1, 5, 2, 1]), "[1, 2, 5]")
-console.log(uniqueSort2([4, 2, 2, 3, 2, 2, 2]), "[2, 3, 4]")
+console.log(uniqueSort2([1, 5, 2, 1]), "[1, 2, 5]");
+console.log(uniqueSort2([4, 2, 2, 3, 2, 2, 2]), "[2, 3, 4]");
--- 04_memoization.js
+++ 04_memoization.js
@@ -7,7 +7,7 @@
 // console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~');
 // console.log('times10 returns:', times10(9));
 
-// Task 2: Use an object to cache the results of your times10 function. 
+// Task 2: Use an object to cache the results of your times10 function.
 // protip 1: Create a function that checks if the value for n has been calculated before.
 // protip 2: If the value for n has not been calculated, calculate and then save the result in the cache object.
 
@@ -23,7 +23,6 @@
 // console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
 // console.log('Task 2 calculated value:', memoTimes10(9));	// calculated
 // console.log('Task 2 cached value:', memoTimes10(9));	// cached
-
 
 //Task 3: Clean up your global scope by moving your cache inside your function.
 //protip: Use a CLOSURE to return a funciton that you call later
@@ -56,32 +55,30 @@
 
 //protip: Take advantage of the fact that parameters are saved in the closure as well, just like the cache from the previous example.
 
-const times10 = (n) => (n * 10);
+const times10 = (n) => n * 10;
 
 const memoize = (cb) => {
-  let cache = {}
+  let cache = {};
 
   return (n) => {
-    if(n in cache){
-      console.log('Fetching from cache:', n)
-      return cache[n]
+    if (n in cache) {
+      console.log("Fetching from cache:", n);
+      return cache[n];
+    } else {
+      console.log("Calculated value");
+      let result = cb(n);
+      cache[n] = result;
+      return result;
     }
-    else{
-      console.log("Calculated value")
-      let result = cb(n)
-      cache[n] = result
-      return result
-    }
-  }
-}
+  };
+};
 
 //returned function from memoizeAdd
-const memoizedTimes10 = memoize(times10)
-console.log('~~~~~TASK 4~~~~~~~')
-try{
-  console.log('Task 4 calculated value:', memoizedTimes10(9))
-  console.log('Task 4 cached value', memoizedTimes10(9))
-}
-catch(e){
-  console.error('Task 4:', e)
+const memoizedTimes10 = memoize(times10);
+console.log("~~~~~TASK 4~~~~~~~");
+try {
+  console.log("Task 4 calculated value:", memoizedTimes10(9));
+  console.log("Task 4 cached value", memoizedTimes10(9));
+} catch (e) {
+  console.error("Task 4:", e);
 }
